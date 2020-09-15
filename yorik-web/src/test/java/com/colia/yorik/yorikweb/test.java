d1cb7f146b84c60f997dacc36e98ea4cbbf5aafc
colia <colia@foxmail.com>
2020-09-15 14:56:25 +0800

    no message

---
+++ yorik-web/src/test/java/com/colia/yorik/yorikweb/test.java
@@ -0,0 +1,12 @@
+package com.colia.yorik.yorikweb;
+
+import org.springframework.boot.test.context.SpringBootTest;
+
+/**
+ * @Author konglingyao
+ * @Date 2020/9/11
+ */
+@SpringBootTest
+public class test {
+
+}
